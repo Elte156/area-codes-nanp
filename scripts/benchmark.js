d45a6ad1e8845ae36650067ba9f0b0a8e4fae3a3
Tony Pagaduan <Elte156@gmail.com>
2024-04-01 19:43:50 -0400

    benchmarking includes

---
+++ scripts/benchmark.js
@@ -0,0 +1,28 @@
+const areacodes = require('../dist/area_codes')
+
+const Benchmark = require('benchmark');
+const suite = new Benchmark.Suite;
+
+// add tests
+suite
+.add('areacodes 734', function() {
+    const boom = areacodes.areaCodesInService.indexOf('734');
+  })
+.add('areacodes 205', function() {
+    const boom = areacodes.areaCodesInService.indexOf('205');
+})
+
+// add listeners
+.on('cycle', function(event) {
+  console.log(String(event.target));
+})
+.on('complete', function() {
+  console.log('Fastest is ' + this.filter('fastest').map('name'));
+})
+// run async
+.run({ 'async': true });
+
+// logs:
+// => RegExp#test x 4,161,532 +-0.99% (59 cycles)
+// => String#indexOf x 6,139,623 +-1.00% (131 cycles)
+// => Fastest is String#indexOf
