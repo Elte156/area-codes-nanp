4ad199e6fb6c41aa2a11f4c2ef1505f09d51254a
Tony Pagaduan <Elte156@gmail.com>
2024-04-01 21:56:16 -0400

    format

--- scripts/benchmark.js
+++ scripts/benchmark.js
@@ -1,26 +1,26 @@
-const areaCodes = require('../dist/area_codes')
+const areaCodes = require('../dist/area_codes');
 
 const Benchmark = require('benchmark');
-const suite = new Benchmark.Suite;
+const suite = new Benchmark.Suite();
 
 // add tests
 suite
-.add('areaCode check for 734', function() {
+  .add('areaCode check for 734', function () {
     const result = areaCodes.areaCodesInService.indexOf('734');
   })
-.add('areaCode check for 205', function() {
+  .add('areaCode check for 205', function () {
     const result = areaCodes.areaCodesInService.indexOf('205');
-})
-.add('areaCode check for 999', function() {
+  })
+  .add('areaCode check for 999', function () {
     const result = areaCodes.areaCodesInService.indexOf('999');
-})
+  })
 
-// add listeners
-.on('cycle', function(event) {
-  console.log(String(event.target));
-})
-.on('complete', function() {
-  console.log('Fastest is ' + this.filter('fastest').map('name'));
-})
-// run async
-.run({ 'async': true });
+  // add listeners
+  .on('cycle', function (event) {
+    console.log(String(event.target));
+  })
+  .on('complete', function () {
+    console.log('Fastest is ' + this.filter('fastest').map('name'));
+  })
+  // run async
+  .run({ async: true });
